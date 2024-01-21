c8ff813cb17fc30c6a67b835488799e8c4d64297
k11tos <k11tos@nate.com>
2024-01-21 18:42:37 +0900

    01_DocumentGPT.py 업데이트

--- pages/01_DocumentGPT.py
+++ pages/01_DocumentGPT.py
@@ -41,10 +41,10 @@ llm = ChatOpenAI(
 @st.cache_data(show_spinner="Embedding file...")
 def embed_file(file):
     file_content = file.read()
-    file_path = f"pages/.cache/files/{file.name}"
+    file_path = f".\.cache\files\{file.name}"
     with open(file_path, "wb") as f:
         f.write(file_content)
-    cache_dir = LocalFileStore(f"pages/.cache/embeddings/{file.name}")
+    cache_dir = LocalFileStore(f".\.cache\embeddings\{file.name}")
     splitter = CharacterTextSplitter.from_tiktoken_encoder(
         separator="\n",
         chunk_size=600,
